9430f360da040ee4091ea853d1c643834d260695
AKSHAY AGARWAL <agarwal.akshay.akshay8@gmail.com>
2015-09-26 20:24:02 +0530

    Changes added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     
     screen = pygame.display.set_mode((1366,768),pygame.FULLSCREEN)
     pygame.display.set_caption('WordzZap')
-    FPS=60
+    FPS=120
     clock=pygame.time.Clock()
     
     backglist=['./Assets/layer_1.png','./Assets/layer_2.png','./Assets/layer_3.png']
@@ -15,13 +15,22 @@ def main():
     backg = []
     
     character = pygame.image.load('./Assets/1.png')
+    character.convert_alpha()
     
     board=pygame.image.load('./Assets/board1.png')
+    board.convert_alpha()
     
     for img in backglist:
         backg.append(pygame.image.load(img))
+        
     
     backg[2]=pygame.transform.scale(backg[2],(1366,683))
+    
+    for img1 in backg:
+        img1.convert_alpha()
+    xb=40
+    xc=110
+    frames=0
     
     while True:
         
@@ -30,18 +39,23 @@ def main():
                 pygame.quit()
                 sys.exit()
             if event.type==pygame.KEYDOWN:
-                if event.key==  K_ESCAPE:
+                if event.key ==  K_ESCAPE:
                     pygame.quit()
                     sys.exit()
         screen.fill((255,255,255))
-        screen.blit(board,(40,566))
-        screen.blit(character,(100,574))
+        screen.blit(board,(xb,565))
+        screen.blit(character,(xc,573))
         
         screen.blit(backg[2],(0,768-683))
+        
+        if frames%2==0:
+            xb=xb+5
+            xc=xc+5
+        
         clock.tick(FPS)
         pygame.display.update()
         
-        
+        frames=frames+1
         
 if __name__=="__main__":
     main()
