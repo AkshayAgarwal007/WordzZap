09a57fb80ac8b92c40aeb76f3e67dc6eb4deed02
AKSHAY AGARWAL <agarwal.akshay.akshay8@gmail.com>
2015-09-26 20:44:45 +0530

    Files Updated

--- main.py
+++ main.py
@@ -14,22 +14,34 @@ def main():
     
     backg = []
     
-    character = pygame.image.load('./Assets/1.png')
-    character.convert_alpha()
+    character = pygame.image.load('./Assets/1.png').convert_alpha()
+ 
     
-    board=pygame.image.load('./Assets/board1.png')
-    board.convert_alpha()
-    
+    board=pygame.image.load('./Assets/board1.png').convert_alpha()
+        
     for img in backglist:
-        backg.append(pygame.image.load(img))
+        backg.append(pygame.image.load(img).convert_alpha())
         
     
     backg[2]=pygame.transform.scale(backg[2],(1366,683))
+    backg[0]=pygame.transform.scale(backg[0],(1366,683))
+    
+    imglist=[]
+
+    for kp in range(0,4):
+        imglist.append(pygame.image.load("PNG/frame-%s.png"%str(kp+1)).convert_alpha())
+ 
+    for f in range(0,4):
+        imglist[f]=pygame.transform.scale(imglist[f],(100,70))
+        
+  
     
     for img1 in backg:
         img1.convert_alpha()
     xb=40
     xc=110
+    xp=0
+    r=0
     frames=0
     
     while True:
@@ -42,15 +54,30 @@ def main():
                 if event.key ==  K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        screen.fill((255,255,255))
-        screen.blit(board,(xb,565))
-        screen.blit(character,(xc,573))
+        screen.blit(backg[0],(0,0))
+        
+        
         
         screen.blit(backg[2],(0,768-683))
         
-        if frames%2==0:
-            xb=xb+5
-            xc=xc+5
+        screen.blit(board,(xb,565))
+        screen.blit(character,(xc,573))
+        
+        
+        if frames%10==0:
+
+      
+            r=r+1
+            if r==4:
+                r=0
+
+        xp=xp+2
+        screen.blit(imglist[r],(xp,40))
+ 
+        
+        
+        xb=xb+2
+        xc=xc+2
         
         clock.tick(FPS)
         pygame.display.update()
