07fa90d4ad9b88dcff24e172693618d93439c623
AKSHAY AGARWAL <agarwal.akshay.akshay8@gmail.com>
2015-09-28 17:10:06 +0530

    Files Updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from pygame.locals import *
 from main2 import *
 import random
+import string
 
 def text_display(text,color):
    FONT=pygame.font.SysFont('monospace',32)
@@ -10,7 +11,9 @@ def text_display(text,color):
    SURFACEFONT=FONT.render(text,True,color)
    return SURFACEFONT
 
-
+class name:
+    name1=''
+    name2=''
     
 def main():
     pygame.init()
@@ -21,9 +24,7 @@ def main():
     clock=pygame.time.Clock()
     
     red=(255,0,0)
-    a=text_display('GRAPES',red)
-    SURFACER_a=a.get_rect()
-    SURFACER_a.center=(100,150)
+    white=(255,255,255)
     
     backglist=['./Assets/layer_1.png','./Assets/layer_2.png','./Assets/layer_3.png']
     
@@ -81,18 +82,42 @@ def main():
     fall_list=[]
     fall_list_x=[]
     
+   #-------------------------------------------------
+    marker=0
     r1= random.randint(0,len(lst)-1)
     current_string = lst[r1]
+    nms1 = []
+    nms2=[]
     
-    print current_string
+        
+    name.name1=''
+    name.name2=current_string
+    text1=text_display(name.name1,white)
+    text1_a=text1.get_rect()
+
+    text2=text_display(name.name2,red)
+    text2_a=text1.get_rect()
+    
+    FONT=pygame.font.SysFont('monospace',32)
+    FONT.set_bold(True)
+    u=FONT.size(name.name1)
+    
+    text1_a.top=150
+    text1_a.left=40
+    text2_a.top=150
+    text2_a.left=40+u[0]
+    
+    #---------------------------------------------
     sskey=''
-    marker=0
-    string_picker=0
-    fruitobjx=[]
+    
     
     fallx=800
     move=True
     move_right=True
+    
+    fall=False
+    
+    yf=60
    
     
     while True:
@@ -104,29 +129,51 @@ def main():
             if event.type==pygame.KEYDOWN:
                 if event.key ==  K_ESCAPE:
                     metro2(screen)
-                    
+                   
                 if event.key <=127:
                     sskey = (chr(event.key))
                     if current_string[marker]==sskey:
                         
                         
                         marker=marker+1
+                        for i4 in range(0,marker):
+                            nms1.append(current_string[i4])
+                        for i5 in range(marker,len(current_string)):
+                            nms2.append(current_string[i5])
+        
+                        name.name1=''.join(nms1)
+                        name.name2=''.join(nms2)
+                        nms1=[]
+                        nms2=[]
                         
                     else:
                         
                         marker=0
+                        name.name1=''
+                        name.name2=current_string
                         
+        print name.name1
+        print name.name2
         
-        if marker == len(display_reg):
+        if marker == len(current_string):
+            fall_list.append(fruit[r1])
+            fall_list_x.append(fs)
+            fall=True
             current_string=''
             marker=0
             r1= random.randint(0,len(lst)-1)
             current_string = lst[r1] 
-            print current_string
+            name.name1=''
+            name.name2=''
+           
+            
             
         
-            
-        print display_reg
+       
+        text1=text_display(name.name1,white)
+        text2=text_display(name.name2,red)
+        
+        
         screen.blit(backg[0],(0,0))
         
         screen.blit(backg[1],(0,768-683))
@@ -136,15 +183,15 @@ def main():
         
         screen.blit(backg[2],(0,768-683))
         screen.blit(fruit[r1],(fs,60))
-        screen.blit(a,SURFACER_a)
+        
         
         
         screen.blit(board,(xb,565))
         screen.blit(character,(xc,573))
         
-        SURFACER_a.center=(x1,150)
         
-        x1=x1+1
+        
+        
         fs=fs+1
         
         if frames%10==0:
@@ -156,17 +203,38 @@ def main():
 
         xp=xp+1
         screen.blit(imglist[r],(xp,40))
+        FONT=pygame.font.SysFont('monospace',32)
+        FONT.set_bold(True)
+        u=FONT.size(name.name1)
+        
+        text1_a.left=text1_a.left+1
+        text2_a.left=text1_a.left+u[0]
+        
  
-       
+        screen.blit(text1,text1_a)
+        screen.blit(text2,text2_a)
+        
+        if fall==True:
+            for imgh in fall_list:
+                screen.blit(imgh,(fall_list_x[0],yf))
+                yf=yf+1
+        
+                if yf+70>565+190:
+                    fall=False
+                    fallx=fall_list_x[0]
+                    move=True
+                    move_right=False
+                    
         if xp>1450:
             xp=-200
-            current_string=[]
-            display_reg=[]
+            current_string=''
             marker=0
+            
             r1= random.randint(0,len(lst)-1)
-            current_string = list(lst[r1]) 
-            for r3 in current_string:
-                display_reg.append(0)
+            current_string = lst[r1]
+            name.name1=''
+            name.name2=current_string
+           
             
         
         
